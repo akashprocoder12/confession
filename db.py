8ea85de8bf3294e6a5ad7df898d53aa7c7bf9cee
akash pandey <akashpandey23aids@rnsit.ac.in>
2025-04-19 21:22:10 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
+import os
 from pymongo import MongoClient
 
-import os
+MONGO_URL = os.environ.get("MONGO_URL")  # Must match Railway variable
+client = MongoClient(MONGO_URL)
 
-
-client = MongoClient("mongodb+srv://proversionofakash:yXmPmDDhLvZfSx62@cluster0.eiefq2m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.confessionDB
 confessions = db.confessions
